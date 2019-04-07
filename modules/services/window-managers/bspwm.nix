2e6b30e2316e7b52c41d57cf65ead09df7f1b067
Agatha Heterodyne <sterling.dorminey@gmail.com>
2019-04-07 01:45:20 -0700

    It works, but sxhkd is really slow.

--- modules/services/window-managers/bspwm.nix
+++ modules/services/window-managers/bspwm.nix
@@ -6,10 +6,44 @@ let
   cfg = config.xsession.windowManager.bspwm;
   bspwm = cfg.bspwm-package;
   sxhkd = cfg.sxhkd-package;
+
+  start-sxhkd = pkgs.writeShellScriptBin "start-sxhkd" ''
+    . "${config.home.profileDirectory}/etc/profile.d/hm-session-vars.sh"
+    ${sxhkd}/bin/sxhkd &
+  '';
+
+  # Adapted from ${bspwm}/share/doc/bspwm/examples/bspwmrc:
+  default-bspwmrc = let script = ''
+      bspc monitor -d I II III IV V VI VII VIII IX X
+
+      bspc config border_width         2
+      bspc config window_gap          12
+
+      bspc config split_ratio          0.52
+      bspc config borderless_monocle   true
+      bspc config gapless_monocle      true
+    ''; in writeShellScriptBin "bspwmrc" script;
+
+  default-sxhkdrc = builtins.readFile (builtins.toPath "${bspwm}/share/doc/bspwm/examples/sxhkdrc");
+
 in {
   options = {
     xsession.windowManager.bspwm = {
       enable = mkEnableOption "Bspwm window manager.";
+
+      bspwmrc = mkOption {
+        type = types.path;
+        default = default-bspwmrc;
+        defaultText = "Adapted default bspwmrc.";
+        description = "Config shell script for bspwm.";
+      };
+
+      sxhkdrc = mkOption {
+        type = types.path;
+        default = default-sxhkdrc;
+        defaultText = "Default config file.";
+        description = "Config shell script for sxhkd..";
+      };
 
       bspwm-package = mkOption {
         type = types.package;
@@ -29,11 +63,29 @@ in {
 
   config = mkIf cfg.enable {
     home.packages = [ bspwm sxhkd ];
-    xsession.windowManager = {
-      initExtra = ''
-        ${sxhkd}/bin/sxhkd &
-      '';
-      command = "${bspwm}/bin/bspwm";
+
+    xdg.configFile."bspwm/bspwmrc".source = cfg.bspwmrc;
+    xdg.configFile."sxhkd/sxhkdrc".source = cfg.sxhkdrc;
+
+    xsession.windowManager.command = "${bspwm}/bin/bspwm";
+
+    systemd.user.services.sxhkd = {
+      Unit = {
+        Description = "Simple X Hotkey Daemon";
+        After = [ "graphical-session-pre.target" ];
+        PartOf = [ "graphical-session.target" ];
+        X-Restart-Triggers = [ config.xdg.configFile."sxhkd/sxhkdrc".source ];
+      };
+
+      Service = {
+        Type = "forking";
+#        Environment = "PATH=${cfg.sxhkd-package}/bin:${cfg.bspwm-package}/bin:/run/wrappers/bin SHELL=${pkgs.runtimeShell}";
+        ExecStart = "${start-sxhkd}/bin/start-sxhkd";
+      };
+
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
     };
   };
 }
